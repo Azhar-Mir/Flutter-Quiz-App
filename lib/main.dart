5ac484567c16b5c5e87adc06c240f2bd54520987
Azhar <mirazhar75@gmail.com>
2021-06-25 16:49:29 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+//void main() {
+//  runApp(MyApp());
+//}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'Yellow'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Lion', 'Panthar', 'Tiger', 'Leapord'],
+    },
+    {
+      'questionText': 'What\'s your favorite programming language?',
+      'answers': ['C++', 'Java', 'Python', 'Javascript'],
+    },
+  ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          backgroundColor: Color(0xFFF44336),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
+    );
+  }
+}
